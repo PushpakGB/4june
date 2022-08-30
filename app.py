3088a5259b6714b31cac62ab284b3218e995fa0d
PushpakB <pushpakbhusari8695@gmail.com>
2022-08-30 11:23:42 +0530

    Connection to DB and prediction APi included

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ app=Flask(__name__)
 @app.route('/')
 def index():
     return "default api"
+@app.route('/predict', methods=['GET','POST'])
+def predict(    ):
+    #Prediction logic
+    return " api"
+
+
+
 
 if __name__=="__main__":
     app.run(debug=='True')
